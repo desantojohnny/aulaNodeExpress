024a50abe4ed5478fde1a5a91857d2de7e2e15a6
desantojohnny <johnny.santo@gmail.com>
2022-03-04 22:04:41 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+
 const { append } = require('express/lib/response');
 
 const server  = express()
+
+const bodyParser = require('body-parser')
+
+const jsonParser = bodyParser.json()
 
 const usuarios = [{id:1, nome:"Johnny", sobrenome:"Santo"}, {id:2, nome:"Bruna", sobrenome:"Santo"}];
 
@@ -17,7 +22,21 @@ server.get("/:id", (req,res) => {
     res.send(aux);
 })
 
+server.post('/usuarios', jsonParser, (req, res)=>{
+    console.log(req.body)
+    const newUser = req.body;
+    usuarios.push(newUser)
+    res.send(usuarios)
+})
+
+server.delete('/usuarios/:index', (req,res) =>{
+    const {index} = req.params
+    usuarios.splice(index, 1)
+    res.send(usuarios)
+})
+
 console.log("O serivdor está rodando");
 
 server.listen(3000)
 
+//fazer o 
